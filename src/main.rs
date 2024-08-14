6a43f1749fd1d27abee1f761971281716b389689
opDavi1 <121784309+opDavi1@users.noreply.github.com>
2024-08-13 21:22:41 -0700

    Add usage text
    Add copyright notices to source files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// This file is part of wprn by opDavi1, licensed under the MIT License.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 use std::{env, process};
 use std::path::Path;
 
@@ -5,7 +8,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("Error: Not enough arguments!");
+        wprn::show_usage();
         process::exit(1);
+    } else if args[1] == "--help" {
+        wprn::show_usage();
+        process::exit(0);
     }
 
     let dir = Path::new(&args[1]);
